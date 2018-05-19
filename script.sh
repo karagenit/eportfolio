d0c091776ab1ed5ba46a8572e6a57e9fae2ceb65
Caleb Smith <karagenit@outlook.com>
2018-05-19 16:14:04 -0400

    Remove TODOs

--- script.sh
+++ script.sh
@@ -4,16 +4,13 @@ for i in $(seq 1 1000);
 do
     echo -ne "Trying $i...\t\r"
 
-    # TODO: curl timeout
     data=$(curl https://carmel.instructure.com/eportfolios/$i -I 2>/dev/null)
     status=$(echo $data | grep Status | cut -d$' ' -f2)
+
     if [[ "$status" == "200"  ]];
     then
         data=$(curl https://carmel.instructure.com/eportfolios/$i 2>/dev/null)
         name=$(echo $data | sed -re 's;(.*?)<span id="section-tabs-header-subtitle" class="ellipsis">([^<]*?)</span>(.*?);\2;g')
         echo -e "ID $i Found, Name: $name\t"
     fi
-    # TODO: keep incremeting i until a 404?
 done
-
-# TODO: dump to file
