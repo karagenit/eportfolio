79e0d1b8550191e4fe9b99f0cb70aa66044d41ba
Caleb Smith <karagenit@outlook.com>
2018-05-19 14:34:09 -0400

    Parse Student Name from Page

--- script.sh
+++ script.sh
@@ -3,11 +3,15 @@
 for i in $(seq 1 500);
 do
     echo -ne "Trying $i...\t\r"
+
+    # TODO: curl timeout
     data=$(curl https://carmel.instructure.com/eportfolios/$i -I 2>/dev/null)
     status=$(echo $data | grep Status | cut -d$' ' -f2)
     if [[ "$status" == "200"  ]];
     then
-        echo -e "ID $i Found!\t"
+        data=$(curl https://carmel.instructure.com/eportfolios/$i 2>/dev/null)
+        name=$(echo $data | sed -re 's;(.*?)<span id="section-tabs-header-subtitle" class="ellipsis">([A-Za-z ]*?)</span>(.*?);\2;g')
+        echo -e "ID $i Found, Name: $name\t"
     fi
     # TODO: keep incremeting i until a 404?
 done
