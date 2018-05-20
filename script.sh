dcecf2af985bc7b2d4b83e6cb36a168cbf8d79eb
Caleb Smith <karagenit@outlook.com>
2018-05-20 18:43:17 -0400

    Write Results to File
    Closes #2

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "ID,Title,Author" > output.csv
+
 for i in $(seq 1 1000);
 do
     echo -ne "Trying $i...\t\r"
@@ -12,6 +14,8 @@ do
         data=$(curl https://carmel.instructure.com/eportfolios/$i 2>/dev/null | tr '\n' ' ')
         title=$(echo $data | sed -re 's;(.*?)<div class="eportfolio-name"> <h3>([^<]*?)</h3>(.*?);\2;g')
         name=$(echo $data | sed -re 's;(.*?)<span id="section-tabs-header-subtitle" class="ellipsis">([^<]*?)</span>(.*?);\2;g')
-        echo -e "ID $i Found, Title: $title, Name: $name\t"
+        echo -ne "          \r" # Clears the "trying..." junk
+        echo -e "[$i]\t$title by $name"
+        echo "$i,$title,$name" >> output.csv
     fi
 done
