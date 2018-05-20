4b4853bb837e951077985381041e2673c719acac
Caleb Smith <karagenit@outlook.com>
2018-05-20 18:34:41 -0400

    Parse Page Title Too
    Closes #4

--- script.sh
+++ script.sh
@@ -9,8 +9,9 @@ do
 
     if [[ "$status" == "200"  ]];
     then
-        data=$(curl https://carmel.instructure.com/eportfolios/$i 2>/dev/null)
+        data=$(curl https://carmel.instructure.com/eportfolios/$i 2>/dev/null | tr '\n' ' ')
+        title=$(echo $data | sed -re 's;(.*?)<div class="eportfolio-name"> <h3>([^<]*?)</h3>(.*?);\2;g')
         name=$(echo $data | sed -re 's;(.*?)<span id="section-tabs-header-subtitle" class="ellipsis">([^<]*?)</span>(.*?);\2;g')
-        echo -e "ID $i Found, Name: $name\t"
+        echo -e "ID $i Found, Title: $title, Name: $name\t"
     fi
 done
